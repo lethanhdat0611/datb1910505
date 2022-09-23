e0a0d4e94890dbad2cc847883e95c2025944102f
lethanhdat0611 <113762542+lethanhdat0611@users.noreply.github.com>
2022-09-23 17:17:43 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,6 +9,6 @@ app.get("/", (req, res)=>{
     res.json({ message: " Welcome to contact book application."
 });
 });
-
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
